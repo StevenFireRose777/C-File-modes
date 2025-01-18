fbaff74c714216ac9a70c0869ff28cb4be9e5639
StevenFireRose777 <stevennuno769@gmail.com>
2025-01-17 22:33:20 -0800

    Updated file.c

--- file/file.c
+++ file/file.c
@@ -6,14 +6,14 @@
 int main(int argc, char* argv[]) {
 
     if(argc == 0){
-        perror("ANGRY BUTNER!");
+        perror("WHERE IS THE INPUT!?");
         return 1;
     }
 
     FILE* fptr;
     if(strcmp(argv[1], "file") == 0){
         fptr = fopen("newfile.txt", "w");
-        fprintf(fptr, "Hello Butner.\n");
+        fprintf(fptr, "Hello I am Steven and I like trains.\n");
         fclose(fptr);
     }else if(strcmp(argv[1],"delete") == 0){
         const char *file = "newfile.txt";
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
             if(remove(file) == 0){
                 printf("Bye bye\n");
             }else{
-                perror("FUCK THERES AN ERROR, FILE STILL HERE!!!!");
+                perror("THERES AN ERROR, FILE STILL HERE!!!!");
             }
         }
     }else if(strcmp(argv[1], "append") == 0){
@@ -35,7 +35,7 @@ int main(int argc, char* argv[]) {
 
         
     }else{
-        perror("BUTNER MADDD!!");
+        perror("PROGRAM AINT HAPPY");
         return 1;
     }   
 
